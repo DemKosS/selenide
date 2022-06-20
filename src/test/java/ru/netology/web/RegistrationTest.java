4d3cf9619ba40b38bdba61c85117c0f62f7b7571
DemKosS <sidyakinkn@gmail.com>
2022-06-20 20:44:30 +0300

    change

--- src/test/java/ru/netology/web/RegistrationTest.java
+++ src/test/java/ru/netology/web/RegistrationTest.java
@@ -1,34 +1,31 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Configuration;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
-import java.time.Duration;
+
+
+
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RegistrationTest {
-    @Test
-    void shouldRegisterByAccountNumberDOMModification() {
-        open("http://localhost:9999");
-        $$(".tab-item").find(exactText("По номеру счёта")).click();
-        $("[name='number']").setValue("4055 0100 0123 4613 8564");
-        $("[name='phone']").setValue("+792000000000");
-        $$("button").find(exactText("Продолжить")).click();
-        $(withText("Успешная авторизация")).shouldBe(visible, Duration.ofMillis(5000));
-        $(byText("Личный кабинет")).shouldBe(visible, Duration.ofMillis(5000));
-    }
+
 
     @Test
-    void shouldRegisterByAccountNumberVisibilityChange() {
-        open("http://localhost:9999");
-        $$(".tab-item").find(exactText("По номеру счёта")).click();
-        $$("[name='number']").last().setValue("4055 0100 0123 4613 8564");
-        $$("[name='phone']").last().setValue("+792000000000");
+    void shouldRegisterByAccount() {
+        Configuration.holdBrowserOpen = true;
+        open("http://0.0.0.0:9999/");
+        $x("(//input[contains(@class,'input__control')])[1]").setValue("Иванов Иван");
+        $("[name='phone']").setValue("+792000000000");
+        $("[data-test-id=agreement]>.checkbox__box").click();
         $$("button").find(exactText("Продолжить")).click();
-        $(withText("Успешная авторизация")).shouldBe(visible, Duration.ofSeconds(15));
-        $(byText("Личный кабинет")).shouldBe(visible, Duration.ofSeconds(15));
+
+
     }
 }
 
